37f79dfbec6557d82f1cf20de0c1e71774cbceda
Luke Harris <luke+git@lkhrs.com>
2024-04-01 15:19:23 -0500

    Add support for comparing two dates

--- main.go
+++ main.go
@@ -8,6 +8,17 @@ import (
 	"time"
 )
 
+func dateCheck(input string) time.Time {
+	if input == "" {
+		input = time.Now().Format(time.DateOnly)
+	}
+	date, err := time.Parse(time.DateOnly, input)
+	if err != nil {
+		panic(err)
+	}
+	return date
+}
+
 func prompt(prompt string) (input string) {
 	r := bufio.NewReader(os.Stdin)
 	fmt.Fprintf(os.Stderr, prompt)
@@ -16,19 +27,18 @@ func prompt(prompt string) (input string) {
 	return
 }
 
-func timeElapsed(input string) (years float64) {
-	date, err := time.Parse(time.DateOnly, input)
-	if err != nil {
-		panic(err)
-	}
-	elapsed := time.Since(date).Minutes()
+func timeElapsed(first, second string) (firstDate, secondDate time.Time, years float64) {
+	firstDate = dateCheck(first)
+	secondDate = dateCheck(second)
+	elapsed := secondDate.Sub(firstDate).Minutes()
 	years = elapsed / 60 / 24 / 365
 	return
 }
 
 // TODO: Print "It has been x years, x months, x days, and x minutes since date."
 func main() {
-	input := prompt("Enter date (YYYY-MM-DD): ")
-	years := timeElapsed(input)
-	fmt.Printf("%.2f years since %s", years, input)
+	firstDate := prompt("Enter date (YYYY-MM-DD): ")
+	secondDate := prompt("Enter second date (press enter for today): ")
+	first, second, years := timeElapsed(firstDate, secondDate)
+	fmt.Printf("%.1f years between %s and %s", years, first.Format(time.DateOnly), second.Format(time.DateOnly))
 }
--- readme.md
+++ readme.md
@@ -7,5 +7,6 @@ A little Go utility to compute the time elapsed since a given date.
 ```shell
 $ go run main.go
 Enter date (YYYY-MM-DD): 2020-02-29
-4.09 years since 2020-02-29
+Enter second date (press enter for today): ⏎
+4.1 years between 2020-02-29 and 2024-04-01
 ```
